4fa1917fa44d86618feecc536f8c554c93a729ae
s.ditaranto <simodita99@gmail.com>
2024-02-22 16:02:37 +0100

    milestone 2 completata

--- js/script.js
+++ js/script.js
@@ -168,11 +168,54 @@ createApp({
                         }
                     ],
                 }
-            ]
-            
+            ],
 
+            // memorizzo in una variabile il contatto attuale
+            currentElement: {
+                name: 'Michele',
+                avatar: './img/avatar_1.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
+            },
         }
 
+
+
     },
+
+    methods: {
+
+
+        indexNumber(currendContact) {
+            
+            this.currentElement = currendContact;
+
+        },
+
+        // getMessageTime(date) {
+        //     console.log(date)
+        //     const newDate = date.split();
+        //     console.log(newDate)
+            
+
+        // }
+
+    }
 
 }).mount("#app");
